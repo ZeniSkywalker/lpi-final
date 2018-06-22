bfcce4033b026ed30ce953248b938242486eea5e
ZeniSkywalker <kaiesanduarte@gmail.com>
2018-06-22 05:17:14 -0300

    Update ConexaoBD.java

--- src/classes/ConexaoBD.java
+++ src/classes/ConexaoBD.java
@@ -12,25 +12,29 @@ package classes;
  */
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class ConexaoBD {
 
-    static {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    public Connection conectar() throws SQLException {
-        String servidor = "localhost";
-        String porta = "3306";
-        String database = "projeto";
-        String usuario = "root";
-        String senha = "root";
-        return DriverManager
-                .getConnection("jdbc:mysql://" + servidor + ":" + porta + "/" + database + "?user=" + usuario + "&password=" + senha);
+    public static Connection conectar() {
+
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+
+        String url = "jdbc:mysql://127.0.0.1:3306/agencia";
+        String user = "root";
+        String password = "root";
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+
+        } catch (Exception e) {
+            //a linha abaixo serve de apoio para esclarecer o erro
+            //System.out.println(e);
+            return null;
+        }
+
     }
 }
